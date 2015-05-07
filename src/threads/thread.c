2655591a0cc95ce409d8ea4a0a1da0f5bf1db7c8
John Chong <zgh1117g@gmail.com>
2015-05-07 14:48:52 +0800

    priority preemptive scheduling

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -210,6 +210,7 @@ thread_create (const char *name, int priority,
 
   /* Add to run queue. */
   thread_unblock (t);
+  thread_yield ();
 
   return tid;
 }
@@ -346,6 +347,7 @@ void
 thread_set_priority (int new_priority) 
 {
   thread_current ()->priority = new_priority;
+  thread_yield ();
 }
 
 /* Returns the current thread's priority. */
